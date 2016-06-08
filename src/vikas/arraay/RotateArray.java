3d679376241e0ab6f0425a63570c648ab9f60218
vikasnar <vikas.n649@gmail.com>
2016-06-07 23:32:46 -0700

    Rotate Array and Array utils

---
+++ src/vikas/arraay/RotateArray.java
@@ -0,0 +1,31 @@
+package vikas.arraay;
+
+
+/**
+ * Created by vnarasimhamurt on 6/2/16.
+ */
+public class RotateArray {
+
+    public static void main(String[] args){
+        int[] arr = new int[]{1,2,3,4,5,6,7};
+        Utils.print(arr);
+        rotate(arr,2);
+        Utils.print(arr);
+    }
+
+    public static void rotate(int[] arr, int d){
+        if(arr == null || arr.length == 0) return;
+        int n = arr.length;
+        if(d >= n) d = d % n;
+        int temp,j;
+        for(int i = 0 ;i < d ; i++){
+            temp = arr[0];
+            for(j = i+1 ; j < n-1 ; j++){
+                arr[j] = arr[j+1];
+            }
+            arr[j] = temp;
+        }
+        Utils.print(arr);
+    }
+
+}
---
+++ src/vikas/arraay/Utils.java
@@ -0,0 +1,16 @@
+package vikas.arraay;
+
+/**
+ * Created by vnarasimhamurt on 6/7/16.
+ */
+public class Utils {
+
+    public static void print(int[] arr){
+        if(arr == null || arr.length ==0)
+            System.out.println("Empty Array!!");
+        for(int i = 0;i< arr.length;i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+}
