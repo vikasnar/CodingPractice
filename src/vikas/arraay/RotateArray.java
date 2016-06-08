228a0791c9cdf52b676dd57e7d1aec3f3dfef803
Vikas <vikas.n649@gmail.com>
2016-06-08 09:16:25 -0700

    Bug Fix: Rotate Array

--- src/vikas/arraay/RotateArray.java
+++ src/vikas/arraay/RotateArray.java
@@ -9,7 +9,7 @@ public class RotateArray {
     public static void main(String[] args){
         int[] arr = new int[]{1,2,3,4,5,6,7};
         Utils.print(arr);
-        rotate(arr,2);
+        rotate(arr,3);
         Utils.print(arr);
     }
 
@@ -20,7 +20,7 @@ public class RotateArray {
         int temp,j;
         for(int i = 0 ;i < d ; i++){
             temp = arr[0];
-            for(j = i+1 ; j < n-1 ; j++){
+            for(j = 0 ; j < n-1 ; j++){
                 arr[j] = arr[j+1];
             }
             arr[j] = temp;
