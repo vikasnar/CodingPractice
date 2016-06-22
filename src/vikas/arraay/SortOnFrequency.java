6d63b22dcf5760d56bb7bbbaae78890c780e866f
Vikas <vikas.n649@gmail.com>
2016-06-21 17:24:37 -0700

    +MergeSort +Sort Based On frequency

---
+++ src/vikas/arraay/SortOnFrequency.java
@@ -0,0 +1,94 @@
+package vikas.arraay;
+
+import org.w3c.dom.Element;
+
+import java.io.InputStream;
+import java.util.*;
+
+/**
+ * Created by vnarasimhamurt on 6/13/16.
+ */
+public class SortOnFrequency {
+    public static void main(String[] args){
+        int[] arr = new int[]{2, 5, 2, 8, 5, 6, 8, 8};
+        Utils.print(arr);
+        sortOnFrequency(arr);
+    }
+
+    public static int[] sortOnFrequency(int[] arr){
+        if(arr == null || arr.length < 2)
+            return arr;
+        List<Tuple> elements = new ArrayList<>();
+        int i;
+        for(i = 0;i<arr.length;i++){
+            elements.add(new Tuple(arr[i],0, i));
+        }
+        Collections.sort(elements, new stablesort());
+        elements.get(0).count = 1;
+        for(i = 1 ;i< arr.length;i++){
+            if(elements.get(i).val == elements.get(i-1).val){
+                elements.get(i).count += elements.get(i-1).count + 1;
+                elements.get(i-1).count = -1;
+            }
+            else{
+                elements.get(i).count = 1;
+            }
+        }
+        Collections.sort(elements, new customSort());
+        i = 0;
+        for(Tuple t : elements){
+            if(t.count != -1)
+                System.out.print(t.val+" ");
+        }
+        return arr;
+    }
+
+    static class Tuple implements Comparable{
+        int val;
+        int count;
+        int idx;
+        public Tuple(int val, int count, int i){
+            this.val = val;
+            this.count = count;
+            this.idx = i;
+        }
+
+        public int compareTo(Object o) {
+            if(o instanceof Tuple){
+                Tuple T = (Tuple) o;
+                if(T.val > this.val)
+                    return -1;
+                else if(T.val < this.val)
+                    return 1;
+                else{
+                    if(T.idx > this.idx)
+                        return 1;
+                    else
+                        return -1;
+                }
+            }
+            return 0;
+        }
+    }
+
+    public static class stablesort implements Comparator<Tuple>{
+        public int compare(Tuple o1, Tuple o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    public static class customSort implements Comparator<Tuple>{
+        public int compare(Tuple o1, Tuple o2)
+        {
+            if(o1.count < o2.count) return 1;
+            else if(o1.count > o2.count) return -1;
+            else {
+                if (o1.idx > o2.idx)
+                    return 1;
+                else
+                    return -1;
+            }
+        }
+    }
+
+}
--- src/vikas/arraay/Utils.java
+++ src/vikas/arraay/Utils.java
@@ -1,17 +1,24 @@
 package vikas.arraay;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.util.Arrays;
+
 /**
  * Created by vnarasimhamurt on 6/7/16.
  */
 public class Utils {
 
+    static int[] array;
+    static int[] temp;
+
     public static void print(int[] arr){
         if(arr == null || arr.length ==0)
             System.out.println("Empty Array!!");
-        for(int i = 0;i< arr.length;i++){
-            System.out.print(arr[i]);
+        for(int i = 0;i< arr.length-1;i++){
+            System.out.print(arr[i]+", ");
         }
-        System.out.println();
+        System.out.println(arr[arr.length-1]);
     }
 
     public static boolean isEmpty(int[] arr) {
@@ -19,4 +26,42 @@ public class Utils {
             return true;
         return false;
     }
+
+    public static int[] sort(int[] arr){
+        array = arr;
+        temp = new int[arr.length];
+        mergeSort(0, arr.length-1);
+        return array;
+    }
+
+    private static void mergeSort( int l, int r){
+        if(l >= r) return;
+        int m = (l+r)/2;
+        mergeSort(l,m);
+        mergeSort(m+1,r);
+        merge(l,m+1,r);
+    }
+
+    private static void merge(int l, int m, int r){
+        for(int i = l; i <= r ; i ++){
+            temp[i] = array[i];
+        }
+        int i = l, j = m, k = l;
+        while(i < m && j <= r){
+            if(temp[i]<temp[j]){
+                array[k++] = temp[i++];
+            }
+            else{
+                array[k++] = temp[j++];
+            }
+        }
+        while(i < m){
+            array[k++] = temp[i++];
+        }
+        while(j <= r){
+            array[k++] = temp[j++];
+        }
+    }
+
+
 }
